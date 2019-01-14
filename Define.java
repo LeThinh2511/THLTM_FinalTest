404ba17c67220f11b63f6eafd1dedf64b38f884c
LeThinh2511 <31402343+LeThinh2511@users.noreply.github.com>
2019-01-14 08:57:06 +0700

    Adding Define.java

---
+++ Define.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thltm_finaltest;
+
+/**
+ *
+ * @author thinhle
+ */
+public class Define {
+    public static int firstRowIndexTeacherList = 1;
+    public static int firstRowIndexRoomList = 1;
+    
+    public static int teacherCodeRowIndex = 1;
+    public static int teacherNameRowIndex = 2;
+    public static int teacherBirthdayRowIndex = 3;
+    public static int teacherSchoolRowIndex = 4;
+    
+    public static int roomNameRowIndex = 1;
+}
